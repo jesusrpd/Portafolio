18183681b6f4a8721df46dbb62f6542a05222aa0
jesusrpd <jesusrodrigo881@gmail.com>
2021-04-06 01:43:13 -0500

    Portafolio casi terminado, solo falta agregar los trabajos e iconos en la seccion habilidades

--- src/pages/Habilidades.jsx
+++ src/pages/Habilidades.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import reactIcon from '../assets/SVG/react.svg';
 
 const Habilidaddes = ()=>(
-    <h1>Habilidades</h1>
+    <div className="habilidades">
+        <h2>Habilidades</h2>  
+        <div className="habilidades-content">  
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+            <div className="habilidad">
+                <img src={reactIcon} alt="react"/>
+                <p>React</p>
+            </div>
+        </div>
+    </div>
 );
 
 export default Habilidaddes;
